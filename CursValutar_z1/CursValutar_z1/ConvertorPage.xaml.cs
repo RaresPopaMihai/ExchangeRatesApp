d068d6384aa350692a047a5e27188bc6e7d2e125
rares <raresmihai.popa@yahoo.com>
2021-10-04 20:01:27 +0300

    Add project files.

---
+++ CursValutar_z1/CursValutar_z1/ConvertorPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CursValutar_z1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConvertorPage : ContentPage
+    {
+        public ConvertorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
