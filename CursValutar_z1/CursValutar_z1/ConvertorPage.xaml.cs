c56d34ee7deb94c8e977e4077ede9308c79dfd5b
Rares Popa <raresmihai.popa@yahoo.com>
2021-10-20 18:57:16 +0300

    Updated with last seminar but incomplete, about 75-80% complete

--- CursValutar_z1/CursValutar_z1/ConvertorPage.xaml.cs
+++ CursValutar_z1/CursValutar_z1/ConvertorPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CursValutar_z1.Models;
 using CursValutar_z1.Services;
+using CursValutar_z1.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,67 +15,24 @@ namespace CursValutar_z1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConvertorPage : ContentPage
     {
-        Dictionary<string, Curs> dictCurs = new Dictionary<string, Curs>();
+        ConvertorViewModel viewModel;
 
+       
         public ConvertorPage()
         {
             InitializeComponent();
-            
+            viewModel = new ConvertorViewModel();
+            BindingContext = viewModel;
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            List<Curs> listaCurs = await SursaDate.ObtineListaCursAsync();
-            Curs ronCurs = new Curs() { Valuta = "RON", Valoare = 1 };
-            dictCurs[ronCurs.Valuta] = ronCurs; 
-            foreach (Curs curs in listaCurs)
-            {
-                dictCurs[curs.Valuta] = curs;
-            }
-
-            pickerValutaDest.ItemsSource = dictCurs.Keys.ToList();
-            pickerValutaSursa.ItemsSource = dictCurs.Keys.ToList();
-            pickerValutaDest.SelectedItem = dictCurs.Keys.First();
-            pickerValutaSursa.SelectedItem = dictCurs.Keys.First();
+           
+            //pickerValutaDest.SelectedItem = "RON";
+            //pickerValutaSursa.SelectedItem = "EUR";
         }
 
-        private void ConvertesteButon_Clicked(object sender, EventArgs e)
-        {
-            //entrySuma -> valoare de convertit 200
-            //pickerValutaSursa -> selectare valuta sursa EUR
-            //pickerValutaDest -> selectare valuta destinatie (conversie) USD
-
-            //labelRezultat -> valoare rezultat
-            //1. Preluare valoare de convertit (entrySuma.Text: suma) -> 200
-            double suma = 0;
-            double.TryParse(entrySuma.Text, out suma);
-
-            //2. Preluare valuta sursa (pickerValutaSursa.SelectedItem) -> "EUR"
-
-            string valutaSursa = (string)pickerValutaSursa.SelectedItem;
-
-            //2.1 Obtinere curs asociat (cursSUrsa: obiect Curs valuta sursa) -> obiect Curs{Valuta ="EUR"}
-
-            Curs cursSursa = dictCurs[valutaSursa];
-
-            //3 Preluare valuta destinatie (pickerValutaSursa.SelectedItem) -> "USD"
-
-            string valutaDest = (string)pickerValutaDest.SelectedItem;
-
-            //3.1 Obtinere curs asociat (cursDest:obiect Curs valuta sursa ) -> obiect Curs{Valuta = "USD"}
-            Curs cursDest = dictCurs[valutaDest];
-
-            //4. Calculam rezultatul
-            //Cati euro cumpar cu 200 de lei?
-            //rezultat = suma * cursSursa.Multiplicator * cursSursa.Valoare / cursDest.Multiplicator * cursDest.Valoare;
-
-            double rezultat = suma * cursDest.Multiplicator * cursSursa.Valoare / (cursSursa.Multiplicator * cursDest.Valoare);
-
-
-            //Afisare rezultat
-            labelRezultat.Text = rezultat.ToString();
-
-        }
+        
     }
 }
---
+++ CursValutar_z1/CursValutar_z1/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CursValutar_z1.Models;
+using CursValutar_z1.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursValutar_z1.ViewModels
+{
+    class MainViewModel
+    {
+        public List<Curs> ListaCurs { get; set; }
+        public string Data { get; set; }
+
+        public MainViewModel()
+        {
+            InitializareLista();
+            if(ListaCurs.Count>0)
+                Data = ListaCurs[0].Data;
+        }
+
+        public async void InitializareLista()
+        {
+            ListaCurs = await SursaDate.ObtineListaCursAsync();
+          
+        }
+    }
+}
